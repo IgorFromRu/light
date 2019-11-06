89f1fadbb0490693916c3fd79ec770859d99e5ca
Igor Romanov <yyy1407198888@gmail.com>
2019-11-06 18:07:00 +0400

    MessageMapper

--- src/main/java/com/street/light/controllers/MessageController.java
+++ src/main/java/com/street/light/controllers/MessageController.java
@@ -1,4 +1,23 @@
 package com.street.light.controllers;
 
+import com.street.light.dto.MessageDto;
+import com.street.light.mapper.MessageMapper;
+import com.street.light.services.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("messages")
 public class MessageController {
+
+    @Autowired
+    private MessageMapper messageMapper;
+
+    @Autowired
+    private MessageService messageService;
+
+    @GetMapping("/{id}")
+    public MessageDto getById(@PathVeriable("id") int id){
+
+    }
 }
--- src/main/java/com/street/light/mapper/MessageMapper.java
+++ src/main/java/com/street/light/mapper/MessageMapper.java
@@ -1,4 +1,27 @@
 package com.street.light.mapper;
 
+import com.street.light.dto.MessageDto;
+import com.street.light.model.Message;
+
 public class MessageMapper {
+
+    public MessageDto dto(Message message){
+        MessageDto messageDto = new MessageDto();
+        messageDto.setId(message.getId());
+        messageDto.setCreateTime(message.getCreateTime());
+        messageDto.setText(message.getText());
+        messageDto.setUserId(message.getUserId());
+        messageDto.setMessageId(message.getMessageId());
+        return messageDto;
+    }
+
+    public Message model(MessageDto messageDto){
+        Message message = new Message();
+        message.setId(messageDto.getId());
+        message.setCreateTime(messageDto.getCreateTime());
+        message.setText(messageDto.getText());
+        message.setUserId(messageDto.getUserId());
+        message.setMessageId(messageDto.getMessageId());
+        return message;
+    }
 }
