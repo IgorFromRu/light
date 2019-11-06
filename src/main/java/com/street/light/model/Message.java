025821ca7390d3f63900eea41f355f96587d28dd
Igor Romanov <yyy1407198888@gmail.com>
2019-11-06 17:29:21 +0400

    massage & massageDto

---
+++ src/main/java/com/street/light/model/Message.java
@@ -0,0 +1,51 @@
+package com.street.light.model;
+
+import java.time.LocalDateTime;
+
+public class Message {
+    int id;
+    LocalDateTime createTime;
+    String text;
+    int userId;
+    int messageId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(int messageId) {
+        this.messageId = messageId;
+    }
+}
